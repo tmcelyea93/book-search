14f16e4a97782a8b6a0e74365193d0fb8f99ff12
tmcelyea93 <tarahmcelyea@gmail.com>
2020-11-02 21:48:03 -0600

    signup and login

--- Develop/client/src/components/SignupForm.js
+++ Develop/client/src/components/SignupForm.js
@@ -3,6 +3,8 @@ import { Form, Button, Alert } from 'react-bootstrap';
 
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { ADD_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/react-hooks';
 
 const SignupForm = () => {
   // set initial form state
@@ -11,6 +13,8 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  const [addUser] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,15 +32,15 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const { data } = await addUser({ variables: { ...userFormData} });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+     // if (!response.ok) {
+       // throw new Error('something went wrong!');
+     // }
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+    //  const { token, user } = await response.json();
+    //  console.log(user);
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
