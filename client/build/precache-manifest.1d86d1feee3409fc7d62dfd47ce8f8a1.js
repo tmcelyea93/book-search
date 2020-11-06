ff1d826c2af4ab01f90023f627c3bc9fa6b638ff
tmcelyea93 <tarahmcelyea@gmail.com>
2020-11-05 21:27:08 -0600

    updating for heroku

---
+++ client/build/precache-manifest.1d86d1feee3409fc7d62dfd47ce8f8a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a301bf072d117427791cf7eab39f935f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "577711ae34dfff399fff",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "e9e14f8825b14c94b77d",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "577711ae34dfff399fff",
+    "url": "/static/js/2.8a4e490d.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.8a4e490d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9e14f8825b14c94b77d",
+    "url": "/static/js/main.95ff6a2b.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
